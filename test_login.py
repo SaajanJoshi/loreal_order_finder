b8e9f97eb4feb293cab2989fccf3d1052e09ffe7
Saazan <saajan.n.joshi@gmail.com>
2024-03-15 15:55:23 -0400

    changes to login -> used selenium to get the cookies

--- test_login.py
+++ test_login.py
@@ -1,29 +1,39 @@
-import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-url = "https://ca.lorealpartnershop.com/en/loginform/"
+from selenium.webdriver.common.by import By
 
-payload = 'dwfrm_login_username_d0qdlkozxlbp=david%40cosmeticworld.ca&dwfrm_login_password_d0maagvxplmz=Loreal123!&dwfrm_login_rememberme=true&dwfrm_login_login=SIGN%20IN'
-headers = {
-  'authority': 'ca.lorealpartnershop.com',
-  'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-  'accept-language': 'en',
-  'cache-control': 'max-age=0',
-  'content-type': 'application/x-www-form-urlencoded',
-  'cookie': """""",
-  'origin': 'https://ca.lorealpartnershop.com',
-  'referer': 'https://ca.lorealpartnershop.com/en/login/',
-  'sec-ch-ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
-  'sec-ch-ua-mobile': '?1',
-  'sec-ch-ua-platform': '"Android"',
-  'sec-fetch-dest': 'document',
-  'sec-fetch-mode': 'navigate',
-  'sec-fetch-site': 'same-origin',
-  'sec-fetch-user': '?1',
-  'upgrade-insecure-requests': '1',
-  'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36'
-}
+# Path to the WebDriver executable (replace with your actual path)
+webdriver_path = 'C:\chromedriver\chromedriver.exe'
 
+# Configure Chrome options for headless mode
+chrome_options = Options()
+chrome_options.add_argument('--headless')  # Run in headless mode
 
-response = requests.request("POST", url, headers=headers, data=payload)
+# Create a WebDriver instance (for Chrome)
+driver = webdriver.Chrome(options=chrome_options)
+driver.maximize_window()
+# Navigate to the login page
+driver.get('https://ca.lorealpartnershop.com/en/login/')
 
-print(response.headers['Set-Cookie'].split(";")[0].split("=")[1])
+# Find the username and password fields and enter your credentials
+username_field = driver.find_element(By.CLASS_NAME, "username .accountnumber")
+username_field.send_keys('david@cosmeticworld.ca')  # Replace 'your_username' with your actual username
+
+password_field = driver.find_element(By.CLASS_NAME,'password .input-text')
+password_field.send_keys('Loreal123!')  # Replace 'your_password' with your actual password
+
+remember_me = driver.find_element(By.NAME, "dwfrm_login_rememberme")
+
+driver.execute_script("arguments[0].click();", remember_me)
+
+# Submit the login form
+login_button = driver.find_element(By.NAME,'dwfrm_login_login')
+login_button.click()
+
+# Retrieve cookies
+cookies = driver.get_cookies()
+
+for cookie in cookies:
+    if cookie['name'] == 'dwsid':
+        print(cookie['value'])
